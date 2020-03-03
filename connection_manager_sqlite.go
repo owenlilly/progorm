082ad3fd1437fcd99110515df840f64e5849cb0e
Owen Lilly <owen@bitt.com>
2020-03-02 23:24:44 -0500

    Updated SQLite to extend connectionManager type

--- connection_manager_sqlite.go
+++ connection_manager_sqlite.go
@@ -1,10 +1,6 @@
 package progorm
 
 import (
-	"errors"
-	"sync"
-
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -12,49 +8,14 @@ import (
 const sqLiteDialect = "sqlite3"
 
 type sqliteConnectionManager struct {
-	db         *gorm.DB
-	once       sync.Once
-	debugMode  bool
-	connString string
-}
-
-func (m sqliteConnectionManager) Debug() bool {
-	return m.debugMode
-}
-
-func (m *sqliteConnectionManager) Dialect() string {
-	return sqLiteDialect
-}
-
-func (m *sqliteConnectionManager) ConnString() string {
-	return m.connString
+	ConnectionManager
 }
 
 // Creates an instance of the SQLite implementation of the ConnectionManager interface.
 func NewSQLiteConnectionManager(dbname string, debugMode bool) ConnectionManager {
 	m := &sqliteConnectionManager{
-		connString: dbname,
-		debugMode:  debugMode,
+		ConnectionManager: newConnectionManager(sqLiteDialect, dbname, debugMode),
 	}
 
 	return m
-}
-
-func (m *sqliteConnectionManager) GetConnection() (*gorm.DB, error) {
-	var err error
-	m.once.Do(func() {
-		m.db, err = gorm.Open(sqLiteDialect, m.connString)
-
-		m.db.LogMode(m.debugMode)
-	})
-
-	return m.db, err
-}
-
-func (m sqliteConnectionManager) AutoMigrate(models ...interface{}) error {
-	if m.db != nil {
-		return m.db.AutoMigrate(models...).Error
-	}
-
-	return errors.New("database is nil")
 }
